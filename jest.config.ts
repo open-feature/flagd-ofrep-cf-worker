42540ae5a618934f0fa8df0ddd98caa2110c7cdd
Norris <jonathan.norris@dynatrace.com>
2026-02-27 13:58:55 -0500

    chore: pin js-sdk-contrib PR branch and add local build/test resolution mappings
    Signed-off-by: Norris <jonathan.norris@dynatrace.com>

--- jest.config.ts
+++ jest.config.ts
@@ -6,9 +6,13 @@ const config: Config = {
       displayName: 'js-ofrep-worker',
       preset: 'ts-jest',
       testEnvironment: 'node',
+      roots: ['<rootDir>/packages/js-ofrep-worker', '<rootDir>/contrib/js-sdk-contrib/libs/shared/flagd-core/src'],
       testMatch: ['<rootDir>/packages/js-ofrep-worker/test/**/*.spec.ts'],
       transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: 'packages/js-ofrep-worker/tsconfig.spec.json' }],
+      },
+      moduleNameMapper: {
+        '^@openfeature/flagd-core$': '<rootDir>/contrib/js-sdk-contrib/libs/shared/flagd-core/src/index.ts',
       },
       // Allow ts-jest to transform flagd-core workspace-linked files (ESM exports in .js)
       transformIgnorePatterns: ['/node_modules/(?!@openfeature/)'],
